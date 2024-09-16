710efc8fb2bb6c930f1480af244f5d34a8bb5a6b
Kevin Pita <gitkevin@pm.me>
2024-09-16 19:40:03 +0200

    fix: enable gnome user extensions

--- modules/home/gnome.nix
+++ modules/home/gnome.nix
@@ -6,6 +6,7 @@
     enable = true;
     settings = {
       "org/gnome/shell" = {
+        disable-user-extensions = false;
         enabled-extensions = [ "caffeine@patapon.info" ];
       };
       "org/gnome/shell/extensions/caffeine" = {
