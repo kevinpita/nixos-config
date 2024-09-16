8951af0e6ad86d7a3972aeac41b4fa75774ba6f6
Kevin Pita <gitkevin@pm.me>
2024-09-16 19:48:05 +0200

    feat: open alacritty with super+return

--- modules/home/gnome.nix
+++ modules/home/gnome.nix
@@ -12,8 +12,20 @@
       "org/gnome/shell/extensions/caffeine" = {
         show-indicator = true;
       };
+
       "org/gnome/desktop/interface" = {
         color-scheme = "prefer-dark";
+      };
+
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        custom-keybindings = [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        ];
+      };
+      "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+        name = "Open Alacritty";
+        binding = "<Super>Return";
+        command = "alacritty";
       };
     };
   };
