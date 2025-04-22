2c3ced2b9e92babecc9b1706cec5b59de7f67351
Kevin Pita <gitkevin@pm.me>
2025-04-22 08:51:52 +0200

    fix(vscode): replace deprecated options

--- modules/home/vscode.nix
+++ modules/home/vscode.nix
@@ -4,26 +4,27 @@
   programs.vscode = {
     enable = true;
     package = pkgs.vscodium;
-    extensions = with pkgs.vscode-marketplace; [
-      brettm12345.nixfmt-vscode
-      github.vscode-github-actions
-      golang.go
-      jdinhlife.gruvbox
-      jnoortheen.nix-ide
-      vscodevim.vim
-    ];
+    profiles.default = {
+      extensions = with pkgs.vscode-marketplace; [
+        brettm12345.nixfmt-vscode
+        github.vscode-github-actions
+        golang.go
+        jdinhlife.gruvbox
+        jnoortheen.nix-ide
+        vscodevim.vim
+      ];
 
-    userSettings = {
-      "editor.fontSize" = 18;
-      "editor.fontFamily" = "'Jetbrains Mono', 'monospace', monospace";
-      "telemetry.telemetryLevel" = "off";
-      "files.autoSave" = "afterDelay";
-      "workbench.colorTheme" = "Gruvbox Dark Hard";
-      "editor.formatOnSave" = true;
-      "[nix]" = {
-        "editor.defaultFormatter" = "brettm12345.nixfmt-vscode";
+      userSettings = {
+        "editor.fontSize" = 18;
+        "editor.fontFamily" = "'Jetbrains Mono', 'monospace', monospace";
+        "telemetry.telemetryLevel" = "off";
+        "files.autoSave" = "afterDelay";
+        "workbench.colorTheme" = "Gruvbox Dark Hard";
+        "editor.formatOnSave" = true;
+        "[nix]" = {
+          "editor.defaultFormatter" = "brettm12345.nixfmt-vscode";
+        };
       };
     };
-
   };
 }
