ab47f386df4530a21fbd1d5f897d74a629b0a856
Kevin Pita <gitkevin@pm.me>
2025-06-04 00:34:52 +0200

    chore: update flake.lock

--- modules/nixos/gnome.nix
+++ modules/nixos/gnome.nix
@@ -1,12 +1,10 @@
 { pkgs, ... }:
 {
   services = {
-    xserver = {
-      desktopManager.gnome.enable = true;
-      displayManager.gdm = {
-        enable = true;
-        wayland = true;
-      };
+    desktopManager.gnome.enable = true;
+    displayManager.gdm = {
+      enable = true;
+      wayland = true;
     };
 
     udev.packages = with pkgs; [ gnome-settings-daemon ];
