29cfd0e173ff7133411ab4cbbc5204ccc3f5a9f5
Kevin Pita <gitkevin@pm.me>
2025-08-10 15:45:17 +0200

    feat(ssh): setup ssh agent & github identity file for headless hosts

--- modules/nixos/ssh.nix
+++ modules/nixos/ssh.nix
@@ -1,8 +1,17 @@
 { gui, ... }:
 {
+  programs.ssh = {
+    startAgent = true;
+    extraConfig = ''
+      Host github.com
+        IdentityFile ~/.ssh/id_ed25519
+    '';
+  };
   services.openssh = {
     enable = true;
     settings = {
+      # Even though `!gui` is redundant, as this file is only included for non-GUI systems,
+      # it's here for security reasons, ensuring password authentication is always disabled for gui systems.
       PasswordAuthentication = !gui;
       PermitRootLogin = "no";
     };
