de69b769be2feb0661928f0837721bb527f52369
Kevin Pita <gitkevin@pm.me>
2026-01-28 23:11:17 +0100

    refactor: remove old modules/nixos and modules/home

--- modules/home/jetbrains.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs, ... }:
-{
-  home.packages = with pkgs; [
-    jetbrains.clion
-    jetbrains.datagrip
-    jetbrains.goland
-  ];
-}
--- modules/home/multimedia.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-
-{
-  home.packages = with pkgs; [
-    ffmpeg
-    tidal-hifi
-    vlc
-  ];
-}
