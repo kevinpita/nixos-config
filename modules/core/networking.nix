3a20d82e245d3f94cbdef6a43b0a9bc9b90ac35b
Kevin Pita <gitkevin@pm.me>
2026-01-28 23:07:53 +0100

    fix: add NetworkManager connectivity check for GNOME icons

--- modules/core/networking.nix
+++ modules/core/networking.nix
@@ -3,7 +3,14 @@
 {
   networking = {
     hostName = hostname;
-    networkmanager.enable = true;
+    networkmanager = {
+      enable = true;
+      settings.connectivity = {
+        enabled = true;
+        uri = "http://nmcheck.gnome.org/check_network_status.txt";
+        interval = 300;
+      };
+    };
     nameservers = [
       "1.1.1.1"
       "1.0.0.1"
