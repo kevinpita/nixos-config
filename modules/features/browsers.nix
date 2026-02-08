254561875062c335ac9e40cc1289c7c6bfb3ab11
Kevin Pita <gitkevin@pm.me>
2026-02-08 23:59:31 +0100

    feat: add xdg mimeApps with force overwrite for default apps

--- modules/features/browsers.nix
+++ modules/features/browsers.nix
@@ -13,5 +13,21 @@ lib.mkIf config.features.browsers.enable {
       firefox
       google-chrome
     ];
+
+    xdg.mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "brave-browser.desktop";
+        "x-scheme-handler/http" = "brave-browser.desktop";
+        "x-scheme-handler/https" = "brave-browser.desktop";
+        "x-scheme-handler/about" = "brave-browser.desktop";
+        "x-scheme-handler/unknown" = "brave-browser.desktop";
+        "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+        "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
+        "x-scheme-handler/notion" = "notion-app-enhanced.desktop";
+      };
+    };
+    xdg.configFile."mimeapps.list".force = true;
+    xdg.dataFile."applications/mimeapps.list".force = true;
   };
 }
