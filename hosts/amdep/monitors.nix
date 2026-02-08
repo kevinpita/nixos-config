28944dd33d9fcae440c9776991e88b20b0f11030
Kevin Pita <gitkevin@pm.me>
2026-02-09 00:35:47 +0100

    feat: add dual monitor configuration for amdep

---
+++ hosts/amdep/monitors.nix
@@ -0,0 +1,51 @@
+{
+  username,
+  ...
+}:
+{
+  home-manager.users.${username} = {
+    xdg.configFile."monitors.xml".text = ''
+      <monitors version="2">
+        <configuration>
+          <logicalmonitor>
+            <x>0</x>
+            <y>0</y>
+            <scale>1</scale>
+            <primary>yes</primary>
+            <monitor>
+              <monitorspec>
+                <connector>DP-1</connector>
+                <vendor>AUS</vendor>
+                <product>VG27A</product>
+                <serial>R5LMQS099715</serial>
+              </monitorspec>
+              <mode>
+                <width>2560</width>
+                <height>1440</height>
+                <rate>143.97232</rate>
+              </mode>
+            </monitor>
+          </logicalmonitor>
+          <logicalmonitor>
+            <x>2560</x>
+            <y>0</y>
+            <scale>1</scale>
+            <monitor>
+              <monitorspec>
+                <connector>DP-2</connector>
+                <vendor>AUS</vendor>
+                <product>VG27A</product>
+                <serial>R2LMQS086633</serial>
+              </monitorspec>
+              <mode>
+                <width>2560</width>
+                <height>1440</height>
+                <rate>143.97232</rate>
+              </mode>
+            </monitor>
+          </logicalmonitor>
+        </configuration>
+      </monitors>
+    '';
+  };
+}
