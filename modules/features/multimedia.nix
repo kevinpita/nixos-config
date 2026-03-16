91ea0c24e406a5cdf774f50c32650b38623d86c6
Kevin Pita <gitkevin@pm.me>
2026-03-16 09:35:40 +0100

    feat: add obs-studio to multimedia packages

--- modules/features/multimedia.nix
+++ modules/features/multimedia.nix
@@ -9,6 +9,7 @@ lib.mkIf config.features.multimedia.enable {
   home-manager.users.${username} = {
     home.packages = with pkgs; [
       ffmpeg
+      obs-studio
       tidal-hifi
       vlc
     ];
