04e3e3319b0763e1742947d67b3a397e0e9a8e90
Kevin Pita <gitkevin@pm.me>
2026-05-23 10:50:53 +0200

    chore: always show k8s context

--- modules/core/zsh/p10k.zsh
+++ modules/core/zsh/p10k.zsh
@@ -1312,9 +1312,7 @@
   # typeset -g POWERLEVEL9K_TERRAFORM_VERSION_VISUAL_IDENTIFIER_EXPANSION='⭐'
 
   #############[ kubecontext: current kubernetes context (https://kubernetes.io/) ]#############
-  # Show kubecontext only when the command you are typing invokes one of these tools.
-  # Tip: Remove the next line to always show kubecontext.
-  typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='kubectl|helm|kubens|kubectx|oc|istioctl|kogito|k9s|helmfile|flux|fluxctl|stern|kubeseal|skaffold|kubent|kubecolor|cmctl|sparkctl'
+  # Always show kubecontext (kubie sets KUBECONFIG per-shell, so this reflects the active context).
 
   # Kubernetes context classes for the purpose of using different colors, icons and expansions with
   # different contexts.
