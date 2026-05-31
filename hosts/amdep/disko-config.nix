9318c72bfa722b2b741ed0cf2f03768f570a18be
Kevin Pita <gitkevin@pm.me>
2026-05-31 11:05:13 +0200

    fix(amdep): use stable disko device path

--- hosts/amdep/disko-config.nix
+++ hosts/amdep/disko-config.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/nvme1n1";
+        device = "/dev/disk/by-id/nvme-eui.e8238fa6bf530001001b448b44de0276";
         content = {
           type = "gpt";
           partitions = {
