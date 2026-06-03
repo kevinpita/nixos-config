bc9298c0a22598a8c6bf22480eafdc832dc6d757
Kevin Pita <gitkevin@pm.me>
2026-06-04 00:52:28 +0200

    fix(hulk): network ip

--- hosts/hulk/networking.nix
+++ hosts/hulk/networking.nix
@@ -3,10 +3,10 @@ _: {
   networking.interfaces.eno1 = {
     ipv4.addresses = [
       {
-        address = "192.168.0.3";
+        address = "192.168.1.7";
         prefixLength = 24;
       }
     ];
   };
-  networking.defaultGateway = "192.168.0.1";
+  networking.defaultGateway = "192.168.1.1";
 }
