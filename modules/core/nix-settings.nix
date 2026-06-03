f1ace8be768348048fa69cc95d6ea6db37b19d65
Kevin Pita <gitkevin@pm.me>
2026-06-03 09:23:05 +0200

    feat(nix): add kevinpita cachix binary cache

--- modules/core/nix-settings.nix
+++ modules/core/nix-settings.nix
@@ -6,6 +6,10 @@ _: {
         "nix-command"
         "flakes"
       ];
+      extra-substituters = [ "https://kevinpita.cachix.org" ];
+      extra-trusted-public-keys = [
+        "kevinpita.cachix.org-1:Cu9UtCDSfDq3/WDnI7N1N/LzAh90SPS+1R+nWao/hz0="
+      ];
     };
   };
 
