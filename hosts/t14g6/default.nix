3e03c99cee7164f92928b60eec9be3610aa27203
Kevin Pita <gitkevin@pm.me>
2026-06-17 10:55:58 +0200

    fix(t14g6): allow LUKS to use keyboard

--- hosts/t14g6/default.nix
+++ hosts/t14g6/default.nix
@@ -9,6 +9,15 @@
     ./tlp.nix
     inputs.nixos-hardware.nixosModules.lenovo-thinkpad-t14-amd-gen6
   ];
+
+  # Allow typing the LUKS passphrase on a keyboard attached to the Thunderbolt
+  # dock. Thunderbolt security is set to "user", and the initrd has no boltd, so
+  # authorize Thunderbolt devices there to bring up the dock's USB controller.
+  # (The needed usbhid/hid/xhci/thunderbolt modules are already in the initrd.)
+  boot.initrd.services.udev.rules = ''
+    ACTION=="add", SUBSYSTEM=="thunderbolt", ATTR{authorized}=="0", ATTR{authorized}="1"
+  '';
+
   features = {
     ai.enable = true;
     aws.enable = true;
