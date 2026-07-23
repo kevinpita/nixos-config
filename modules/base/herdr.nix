fa8dd1eba8bdd3d2a434a4982d49fc6c5b641778
Kevin Pita <gitkevin@pm.me>
2026-07-24 01:17:24 +0200

    feat(herdr): minidesk remote alias

--- modules/base/herdr.nix
+++ modules/base/herdr.nix
@@ -137,6 +137,7 @@
           hn = "herdr session attach";
           hp = "herdr session attach peersyst";
           pherdr = "herdr --remote pfium --remote-keybindings server";
+          minidesk = "herdr --remote minidesk --remote-keybindings server";
         };
       };
     };
