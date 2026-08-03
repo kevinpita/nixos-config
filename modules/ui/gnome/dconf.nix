84c2cfb8759bdb7d14865fdd5c396fcc5eef29f9
Kevin Pita <gitkevin@pm.me>
2026-08-03 17:01:35 +0200

    feat(gnome): enable kdeconnect

--- modules/ui/gnome/dconf.nix
+++ modules/ui/gnome/dconf.nix
@@ -22,6 +22,7 @@
                 enabled-extensions = [
                   "caffeine@patapon.info"
                   "clipboard-history@alexsaveau.dev"
+                  "gsconnect@andyholmes.github.io"
                   "tailscale-status@maxgallup.github.com"
                   "claude-usage@dvdstelt.github.io"
                   "codex-usage@kevinpita.dev"
