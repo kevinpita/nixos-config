967a4932af9f105f155709ee5fdfebab952d93b3
Kevin Pita <gitkevin@pm.me>
2026-08-13 10:29:07 +0200

    fix: dni build

--- hosts/t14g6/dni.nix
+++ hosts/t14g6/dni.nix
@@ -17,16 +17,25 @@
     autofirma = {
       enable = true;
       firefoxIntegration.enable = true;
-      package = inputs.autofirma-nix.packages.${pkgs.stdenv.hostPlatform.system}.autofirma.override {
-        buildFHSEnv =
-          args:
-          pkgs.buildFHSEnv (
-            args
-            // {
-              targetPkgs = fhsPkgs: args.targetPkgs fhsPkgs ++ [ fhsPkgs.pcsclite.lib ];
-            }
-          );
-      };
+      package =
+        let
+          upstreamAutofirma = inputs.autofirma-nix.packages.${pkgs.stdenv.hostPlatform.system}.autofirma;
+        in
+        upstreamAutofirma.override {
+          maven-dependencies-hash = "sha256-aNtvfZuu84dS3/ZvbuVlmt2ELQFHr0OtNABnDo/Hdp4=";
+          jmulticard = upstreamAutofirma.clienteafirma.dependencies.jmulticard.override {
+            maven-dependencies-hash = "sha256-2lUqrN8s0KTbk8wd76FkU5wgaPZnzmpO9rgTE6Oe+os=";
+          };
+
+          buildFHSEnv =
+            args:
+            pkgs.buildFHSEnv (
+              args
+              // {
+                targetPkgs = fhsPkgs: args.targetPkgs fhsPkgs ++ [ fhsPkgs.pcsclite.lib ];
+              }
+            );
+        };
     };
 
     firefox.policies.SecurityDevices."OpenSC PKCS#11" = "${pkgs.opensc}/lib/opensc-pkcs11.so";
