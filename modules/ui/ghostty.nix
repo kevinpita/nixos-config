b10da79eb75773a64d6e8a681327576bdef24291
Kevin Pita <gitkevin@pm.me>
2026-08-21 17:46:10 +0200

    refactor(gnome): move keybindings into owning aspects

--- modules/ui/ghostty.nix
+++ modules/ui/ghostty.nix
@@ -24,4 +24,27 @@
         };
       };
     };
+
+  # Ghostty owns its GNOME keybindings; this fragment merges into the gnome
+  # aspect the same way modules/net/syncthing.nix contributes its indicator.
+  flake.modules.nixos.gnome =
+    { lib, username, ... }:
+    {
+      home-manager.users.${username}.dconf.settings = {
+        "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = lib.mkAfter [
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+          "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
+        ];
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+          name = "Open Terminal";
+          binding = "<Super>Return";
+          command = "ghostty";
+        };
+        "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+          name = "Open Pfium Herdr";
+          binding = "F13";
+          command = "ghostty -e herdr --remote pfium --remote-keybindings server";
+        };
+      };
+    };
 }
--- modules/ui/gnome/dconf.nix
+++ modules/ui/gnome/dconf.nix
@@ -1,77 +1,44 @@
 {
   flake.modules.nixos.gnome =
-    {
-      config,
-      lib,
-      username,
-      ...
-    }:
-    let
-      hmConfig = config.home-manager.users.${username};
-      ghosttyEnabled = hmConfig.programs.ghostty.enable;
-      dictationEnabled = lib.any (p: lib.getName p == "dictate-toggle") hmConfig.home.packages;
-    in
+    { username, ... }:
     {
       home-manager.users.${username} = {
         dconf = {
           enable = true;
-          settings = lib.mkMerge [
-            {
-              "org/gnome/shell" = {
-                disable-user-extensions = false;
-                enabled-extensions = [
-                  "caffeine@patapon.info"
-                  "clipboard-history@alexsaveau.dev"
-                  "tailscale-status@maxgallup.github.com"
-                  "claude-usage@dvdstelt.github.io"
-                  "codex-usage@kevinpita.dev"
-                ];
-              };
-              "org/gnome/shell/extensions/caffeine" = {
-                show-indicator = true;
-              };
-              "org/gnome/desktop/interface" = {
-                color-scheme = "prefer-dark";
-                clock-show-seconds = true;
-              };
-              "org/gnome/shell/keybindings" = {
-                show-screenshot-ui = [ "<Super>space" ];
-              };
-              "org/gnome/desktop/wm/keybindings" = {
-                switch-input-source = [ ];
-                switch-input-source-backward = [ ];
-              };
-              "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings =
-                lib.optional ghosttyEnabled "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
-                ++ [ "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/" ]
-                ++ lib.optional dictationEnabled "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
-                ++ lib.optional ghosttyEnabled "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/";
-              "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
-                name = "Emoji Picker";
-                binding = "<Super>period";
-                command = "emoji-picker";
-              };
-            }
-            (lib.mkIf ghosttyEnabled {
-              "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
-                name = "Open Terminal";
-                binding = "<Super>Return";
-                command = "ghostty";
-              };
-              "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
-                name = "Open Pfium Herdr";
-                binding = "F13";
-                command = "ghostty -e herdr --remote pfium --remote-keybindings server";
-              };
-            })
-            (lib.mkIf dictationEnabled {
-              "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
-                name = "Dictation";
-                binding = "<Control><Alt>space";
-                command = "/etc/profiles/per-user/${username}/bin/dictate-toggle";
-              };
-            })
-          ];
+          settings = {
+            "org/gnome/shell" = {
+              disable-user-extensions = false;
+              enabled-extensions = [
+                "caffeine@patapon.info"
+                "clipboard-history@alexsaveau.dev"
+                "tailscale-status@maxgallup.github.com"
+                "claude-usage@dvdstelt.github.io"
+                "codex-usage@kevinpita.dev"
+              ];
+            };
+            "org/gnome/shell/extensions/caffeine" = {
+              show-indicator = true;
+            };
+            "org/gnome/desktop/interface" = {
+              color-scheme = "prefer-dark";
+              clock-show-seconds = true;
+            };
+            "org/gnome/shell/keybindings" = {
+              show-screenshot-ui = [ "<Super>space" ];
+            };
+            "org/gnome/desktop/wm/keybindings" = {
+              switch-input-source = [ ];
+              switch-input-source-backward = [ ];
+            };
+            "org/gnome/settings-daemon/plugins/media-keys".custom-keybindings = [
+              "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+            ];
+            "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+              name = "Emoji Picker";
+              binding = "<Super>period";
+              command = "emoji-picker";
+            };
+          };
         };
       };
     };
