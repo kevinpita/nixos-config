da952d1802d2f7ef436d2ad664bce7eb6b2b88a5
Kevin Pita <gitkevin@pm.me>
2026-09-14 00:52:13 +0200

    fix: avoid globally accepting flake config

--- modules/base/nix-settings.nix
+++ modules/base/nix-settings.nix
@@ -22,7 +22,6 @@
           "nix-command"
           "flakes"
         ];
-        accept-flake-config = true;
         extra-substituters = [ "https://kevinpita.cachix.org" ];
         extra-trusted-public-keys = [
           "kevinpita.cachix.org-1:Cu9UtCDSfDq3/WDnI7N1N/LzAh90SPS+1R+nWao/hz0="
