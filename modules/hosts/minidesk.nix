2bd2862b3b72a977d55024f0a98bd4a814d494f4
Kevin Pita <gitkevin@pm.me>
2026-09-19 12:57:20 +0200

    chore(minidesk): use docker instead of podman

--- modules/hosts/minidesk.nix
+++ modules/hosts/minidesk.nix
@@ -1,7 +1,7 @@
 { config, ... }:
 {
   flake.modules.nixos."hosts/minidesk" =
-    { pkgs, ... }:
+    { lib, pkgs, ... }:
     {
       imports = [
         ../../hosts/minidesk/disko-config.nix
@@ -16,6 +16,11 @@
         config.flake.modules.nixos.work-cloud
         config.flake.modules.nixos.work-github
       ];
+
+      # Rakazo needs Docker API 1.45+ for volume subpaths. Keep Podman running,
+      # but let Docker own its CLI name. Clients select the engine by socket.
+      virtualisation.podman.dockerCompat = lib.mkForce false;
+      virtualisation.docker.rootless.enable = true;
 
       boot.kernelPackages = pkgs.linuxPackages_latest;
       services.moshi-hook.enable = false;
